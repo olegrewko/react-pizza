024ea778efb777322176756bff5f71cbbf426cde
olegrewko <olegrewko@gmail.com>
2022-04-03 13:11:58 +0300

    u

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,19 +1,69 @@
 import React from 'react'
 
- function Categories({items, onClick}) {
-     console.log(items);
+// class Categories extends React.Component {
+//   state = {
+//     activeItem : 3
+//   };
+//   onSelectItem = (index) => {
+//     this.setState({
+//       activeItem: index,
+//     });
+//     this.forceUpdate()
+//   }
+
+//   render() {
+//     const {items, onClickItem } = this.props
+
+//   return(
+//     <div className="categories">
+//                  <ul>
+//                    <li>Все</li>
+               
+//                    {items.map((name, index) => (
+//                   <li 
+//                   className = {this.state.activeItem === index ? 'active' : ''}
+//                    onClick = {() => this.onSelectItem(index)}
+//                     key={`${name}_${index}`}>
+//                     {name}
+//                     </li>
+//                   ))}
+                             
+//                 </ul>
+//               </div>
+//   )
+// }}
+
+ function Categories({items, onClickItem}) {
+     
+     const [activeItem, setActiveItem] =  React.useState(null);
+    //  const state = React.useState(null)
+    //  const activeItem = state[0]
+    //  const setactiveItem = state[1]
+     const onSelectItem = (index) => {
+       setActiveItem(index)
+     }
+    
   return (
    
       <div className="categories">
             <ul>
-              <li className="active">Все</li>
+              <li className = {activeItem === null ? 'active' : ''} 
+              onClick={() => onSelectItem(null)}>Все
+              </li>
            
               {items.map((name, index) => (
-              <li onClick={() => onClick(name)} key={`${name}_${index}`}>{name}</li>
+              <li
+              className = {activeItem === index ? 'active' : ''}
+              onClick={() => onSelectItem(index)}
+               key={`${name}_${index}`}>
+                {name}
+                </li>
               ))}
                          
             </ul>
           </div>
      );
 }
+
+
 export default Categories;
